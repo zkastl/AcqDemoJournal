3e5a2b20df85be885a95a5835472a837fc2578f3
Zachary Kastl <zkastl8@gmail.com>
2017-08-17 14:13:41 -0500

    added better icon

--- AcqDemoJournal/Properties/Resources.Designer.cs
+++ AcqDemoJournal/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace AcqDemoJournal.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon journal_acqdemo {
+            get {
+                object obj = ResourceManager.GetObject("journal_acqdemo", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
